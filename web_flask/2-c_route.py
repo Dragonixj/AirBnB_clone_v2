cc0b08148102064c495442dd7b0d6a64c807513b
Dragonixj <delafiado@gmail.com>
2024-01-27 13:52:54 +0000

    Task 2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -23,7 +23,7 @@ def hbnb():
 @app.route("/c/<string:text>", strict_slashes=False)
 def c_text(text):
     """Displays C"""
-    text = text.replace("_", "")
+    text = text.replace("_", " ")
     return f"C {text}"
 
 
